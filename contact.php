59f263de7b65d0070937e02a31405ca90c9aa346
Nick Pettit <nick@Nicks-MacBook-Pro.local>
2014-11-18 12:29:37 -0500

    Started the contact page.

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php include 'header.php'; ?>
+
+    <section class="row">
+      <div class="small-12 columns">
+        <h2>Contact Us</h2>
+
+        <p>We would love to talk to you about getting connected with the growing Indienomicon community!</p>
+
+        <h3>General Questions</h3>
+        <p>Just have some <em>general questions</em>? Email us at <a href="mailto:hello@indienomicon.com">hello@indienomicon.com</a></p>
+        <h3>Presenting</h3>
+				<p>If you&apos;re interested in <em>Presenting</em> at Indienomicon, contact Dave Sushil by emailing <a href="mailto:david@indienomicon.com">david@indienomicon.com</a> for more information and available dates.</p>
+        <h3>Share Your Story</h3>
+				<p>If you&apos;d like to <em>share our story</em>, we&apos;d be honored! Email Christa at <a href="mailto:christa@indienomicon.com">christa@indienomicon.com</a> to request any resources you may need, or check out our <a href="press.html">Press page</a> for assets and past stories.</p>
+
+      </div>
+    </section>
+
+    <?php include 'mailchimp.php'; ?>
+
+<?php include 'footer.php'; ?>
