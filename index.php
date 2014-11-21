b1865c720783c9f6503d8346ea784f29d10a4285
Nick Pettit <nick@Nicks-MacBook-Pro.local>
2014-11-21 14:48:44 -0500

    Added a cosplay hero image and made a few other changes for the expo.

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
               </ul>
               <div class="expo-sponsors">
                 <h3>Sponsored By</h3>
-                <ul class="small-block-grid-3 logo-grid">
+                <ul class="small-block-grid-3">
                   <li><a href="http://gamesartandmusic.com/"><img src="img/expo/sponsors/sponsor_gaam.png" alt="GAAM: Games Art and Music"></a></li>
                   <li><a href="https://factur.org/"><img src="img/expo/sponsors/sponsor_factur.png" alt="Factur"></a></li>
                   <li><a href="http://tic.ocls.info/"><img src="img/expo/sponsors/sponsor_melrosecenter.png" alt="The Dorthy Lumley Melrose Center: Technology, Innovation, Creativity"></a></li>
@@ -24,7 +24,7 @@
             <div class="small-12 medium-5 columns">
               <div class="next-event content-block">
                 <p class="event-date"><strong>Saturday, December 6th</strong><br>12:00pm - 5:00pm</p>
-                <p class="event-loc"><strong>The Melrose Center <br>at the Downtown Library</strong><br>101 E Central Blvd, Orlando, FL 32801</p>
+                <p class="event-loc"><a href="https://www.google.com/maps/place/Melrose+Center+(Inside+of+Orlando+Public+Library)/@28.542464,-81.377149,17z/data=!3m1!4b1!4m2!3m1!1s0x88e77afc378b4f97:0x88eb726c5efaecd4" title="View on Google Maps" target="new"><strong>The Melrose Center <br>at the Downtown Library</strong><br>101 E Central Blvd, Orlando, FL 32801</a></p>
                 <a href="https://www.facebook.com/events/783183325098945/" class="btn event-btn">Join in!</a>
               </div>
               <p class="event-note">Please note: We will <strong>not</strong> be holding our normal Thursday event in December. See you at the expo!</p>
@@ -36,8 +36,8 @@
     </section>
 
     <section class="expo-features">
-      <a class="expo-box games" href="#featured-games">
-        <h3>Games</h3>
+      <a class="expo-box exhibitors" href="#featured-exhibitors">
+        <h3>Exhibitors</h3>
       </a>
       <a class="expo-box breakouts" href="#breakout-sessions">
         <h3>Breakouts</h3>
@@ -54,9 +54,9 @@
 
     <section class="row">
       <div class="small-12 columns">
-        <h2 id="featured-games">Featured Games and Studios</h2>
+        <h2 id="featured-exhibitors">Featured Games and Studios</h2>
         <p>Play indie games and meet their creators! Even more studios and games are coming, so stay tuned.</p>
-        <ul class="small-block-grid-3 medium-block-grid-5 large-block-grid-6 logo-grid">
+        <ul class="small-block-grid-3 medium-block-grid-5 large-block-grid-6">
           <li><a href="http://phykenmedia.com/"><img src="img/expo/exhibitors/studio_phykenmedia.png" alt="Phyken Media"></a></li>
           <li><a href="http://www.terncraftgames.com/"><img src="img/expo/exhibitors/studio_terncraft.png" alt="Terncraft Games"></a></li>
           <li><a href="http://www.periareion.com/#!tba-games/c5hv"><img src="img/expo/exhibitors/studio_tbagames.png" alt="TBA Games"></a></li>
@@ -98,11 +98,13 @@
       </div>
     </section>
 
-    <section class="row">
-      <div class="small-12 columns">
-        <h2 id="cosplay-contest">Cosplay Contest</h2>
-        <h4>Winners Announced at 4:00pm</h4>
-        <p>Come as your favorite video game character and compete for the grand prize in the first Indienomicon Cosplay Contest! Winners will be announced at 4:00pm. To be eligible for entry, you must <strong>register by 3:00pm</strong></p>
+    <section class="cosplay-hero">
+      <div class="row">
+        <div class="small-12 columns">
+          <h2 id="cosplay-contest">Cosplay Contest</h2>
+          <h4>Winners Announced at 4:00pm</h4>
+          <p>Come as your favorite video game character and compete for the grand prize in the first Indienomicon Cosplay Contest! Winners will be announced at 4:00pm. To be eligible for entry, you must <strong>register by 3:00pm</strong></p>
+        </div>
       </div>
     </section>
 
--- sponsors.php
+++ sponsors.php
@@ -1,11 +1,11 @@
 <section class="row">
   <div class="small-12 columns">
     <h2>Indienomicon Sponsors</h2>
-    <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 logo-grid">
-      <li><a href="http://www.theindiebox.com/" style="background-image: url('img/sponsors/indiebox.png');">The Indie Box</a></li>
-      <li><a href="http://www.terncraftgames.com/" style="background-image: url('img/sponsors/terncraft.png');">Terncraft Games</a></li>
-      <li><a href="http://phykenmedia.com/" style="background-image: url('img/sponsors/phyken.png');">Phyken Media</a></li>
-      <li><a href="http://kaleidoscopeit.com/" style="background-image: url('img/sponsors/kaleidoscope.png');">Kaleidoscope Media</a></li>
+    <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">
+      <li><a href="http://www.theindiebox.com/"><img src="img/sponsors/indiebox.png" alt="The Indie Box"></a></li>
+      <li><a href="http://www.terncraftgames.com/"><img src="img/sponsors/terncraft.png" alt="Terncraft Games"></a></li>
+      <li><a href="http://phykenmedia.com/"><img src="img/sponsors/phyken.png" alt="Phyken Media"></a></li>
+      <li><a href="http://kaleidoscopeit.com/"><img src="img/sponsors/kaleidoscope.png" alt="Kaleidoscope Media"></a></li>
     <ul>
   </div>
 </section>
