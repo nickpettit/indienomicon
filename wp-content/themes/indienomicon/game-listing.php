23a8965a714f0fa9135d066385ead0a6af8ed0e0
Nick Pettit <nick@nickpettit.com>
2015-05-15 14:34:43 -0400

    Fixed an issue with game listings getting cut off.

--- wp-content/themes/indienomicon/game-listing.php
+++ wp-content/themes/indienomicon/game-listing.php
@@ -1,8 +1,8 @@
 <?php $game_logo = get_field('game_logo') ?>
 
 <div class="game row collapse content-block">
-  <div class="small-12 medium-4 large-3 columns game-logo" style="background-image: url('<?php echo $game_logo['url']; ?>')"></div>
-  <div class="small-12 medium-8 large-9 columns">
+  <div class="small-12 medium-4 columns game-logo" style="background-image: url('<?php echo $game_logo['url']; ?>')"></div>
+  <div class="small-12 medium-8 columns">
     <h3 class="game-title"><a href="<?php the_permalink(); ?>"><?php the_field('project_title'); ?></a></h3>
     <h4 class="studio-name">
       <?php if ($studio_website = get_field('studio_website')): ?>
